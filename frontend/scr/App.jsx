4a17b4fd3e2e0dd601c9dc71e68e85af5210c2ed
Carlos Costa <drivecarloscosta@gmail.com>
2025-07-09 23:05:00 -0300

    Configuração inicial do projeto com Docker

---
+++ frontend/scr/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="p-5">
+      <h1>GESM Frontend</h1>
+      <Button variant="primary">Botão de Teste Bootstrap</Button>
+    </div>
+  )
+}
+export default App
